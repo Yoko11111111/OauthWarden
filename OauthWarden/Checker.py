49aee82251de279e47aaea3e2c70253c61698511
Yokozuna / Zuna <108374464+Yoko11111111@users.noreply.github.com>
2023-03-14 12:33:17 +0100

    Update Checker.py

--- OauthWarden/Checker.py
+++ OauthWarden/Checker.py
@@ -1,1 +1,12 @@
+from OauthWarden import warden
 
+def test_check_token():
+    # create a test token file
+    with open("tokens.txt", "w") as f:
+        f.write("this is a test")
+
+    # call the function to check the test file
+    warden.checktoken("tokens.txt")
+
+    # delete the test file after the check is complete
+    os.remove("tokens.txt")
